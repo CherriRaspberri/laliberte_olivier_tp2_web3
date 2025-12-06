65e9b612a68aa367ff1751c24bee4cd3dcd60280
CherriRaspberri <103427209+CherriRaspberri@users.noreply.github.com>
2025-12-06 16:30:34 -0500

    v0.2
    Added : zdog
    - zdog animation (spinning square + dots)
    - Fixed 'now playing' animations

--- src/js/typedjs.js
+++ src/js/typedjs.js
@@ -1,8 +1,23 @@
 import Typed from "typed.js";
 
-new Typed("#play-pause", {
-    strings: ["RETRO-FUTURIST", "NOW PLAYING..."],
+let isPlaying = false;
+
+let playText = new Typed("#play-pause", {
+    strings: ["RETRO-FUTURIST"],
     typeSpeed: 50,
     backSpeed: 50,
     backDelay: 3000
+});
+
+document.getElementById('play-button').addEventListener('click', function() {
+    if (!isPlaying) {
+        playText.strings = ["NOW PLAYING..."];
+        playText.reset(true);
+        isPlaying = true
+    }
+    else {
+        playText.strings = ["PAUSED"];
+        playText.reset(true);
+        isPlaying = false;
+    }
 });
