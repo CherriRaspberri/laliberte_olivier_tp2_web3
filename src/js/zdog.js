b4699890cfde4653ee3954ad4fbe2006ae1bc811
CherriRaspberri <103427209+CherriRaspberri@users.noreply.github.com>
2025-12-07 03:03:30 -0500

    v0.3
    Added:
    - tone-visualizer (pckg)
    - Speaker (CSS)
    - Amp (CSS)

--- src/js/zdog.js
+++ src/js/zdog.js
@@ -2,57 +2,61 @@ import Zdog from "zzz";
 
 let isSpinning = true;
 
+//canvas
 const illo = new Zdog.Illustration({
-    element: ".zdog-canvas",
-    rotate: { x: -Zdog.TAU / 16 },
-    dragRotate: true,
-    onDragStart: function() {
-        isSpinning = false;
-    },
-    onDragEnd: function() {
-        isSpinning = true;
-    }
+  element: ".zdog-canvas",
+  rotate: { x: -Zdog.TAU / 16 },
+  dragRotate: true,
+  onDragStart: function () {
+    isSpinning = false;
+  },
+  onDragEnd: function () {
+    isSpinning = true;
+  },
 });
 
+//Rectangle
 new Zdog.Rect({
+  addTo: illo,
+  width: 100,
+  height: 100,
+  stroke: 16,
+  fill: true,
+  color: "#f4f2ef",
+});
+
+//Dots pattern
+[false, true].forEach(function (isGroup) {
+  let SliceClass = isGroup ? Zdog.Group : Zdog.Anchor;
+
+  let dotSlice = new SliceClass({
     addTo: illo,
-    width: 100,
-    height: 100,
-    stroke: 16,
-    fill: true,
-    color: "#f2d6b1"
+    translate: { z: isGroup ? 25 : -25 },
+  });
+
+  const d = 45;
+
+  let dot = new Zdog.Shape({
+    addTo: dotSlice,
+    stroke: 20,
+    color: isGroup ? "#7a736d" : "#556149",
+  });
+
+  dot.copy({ translate: { x: -d, y: -d } });
+  dot.copy({ translate: { x: 0, y: -d } });
+  dot.copy({ translate: { x: d, y: -d } });
+  dot.copy({ translate: { x: -d, y: 0 } });
+  dot.copy({ translate: { x: d, y: 0 } });
+  dot.copy({ translate: { x: -d, y: d } });
+  dot.copy({ translate: { x: 0, y: d } });
+  dot.copy({ translate: { x: d, y: d } });
 });
 
-[false, true].forEach(function(isFixed) {
-    let group = new Zdog.Group({
-        addTo: illo,
-        translate: { z: isFixed ? 25 : -25 }
-    });
-
-    const d = 45;
-    let x = d * ( isFixed ? -1 : 1 );
-    
-    //dot
-    new Zdog.Shape({
-        addTo: group,
-        stroke: 20,
-        translate: { x: x, y: -d },
-        color: isFixed ? '#39613a' : '#1f301f'
-    });
-
-    if (isFixed) {
-        new Zdog.Shape({
-            addTo: group,
-            translate: {x: x, y: d}
-        })
-    }
-});
-
+//Canvas spin
 function animate() {
-    illo.rotate.y += isSpinning ? 0.01 : 0;
-
-    illo.updateRenderGraph();
-    requestAnimationFrame(animate);
+  illo.rotate.y += isSpinning ? 0.01 : 0;
+  illo.zoom = 0.9;
+  illo.updateRenderGraph();
+  requestAnimationFrame(animate);
 }
-
 animate();
